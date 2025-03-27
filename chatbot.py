9134eaf6502881045a21e9b0fd6f13a3d123a37a
Quentin Cartier <daoliangshumaishuji@gmail.com>
2025-03-27 21:40:17 +0100

    Generate fist output from test input

--- chatbot.py
+++ chatbot.py
@@ -6,3 +6,20 @@ model_name = "facebook/blenderbot-400M-distill"
 # Load model (download on first run and reference local installation for consequent runs)
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+# Keeping track of conversation history
+conversation_history = []
+
+history_string = "\n".join(conversation_history)
+
+input_text ="hello, how are you doing?"
+
+inputs = tokenizer.encode_plus(history_string, input_text, return_tensors="pt")
+print(inputs)
+print(tokenizer.pretrained_vocab_files_map)
+
+outputs = model.generate(**inputs)
+print(outputs) # A dictionary containing the generated tokens ( not text)
+
+response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
+print(response)
